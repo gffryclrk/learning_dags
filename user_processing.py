50edfeff783e2c88416d7079a5f80622f5fda97a
Geoffrey Clark <g.clark@samsung.com>
2022-02-09 09:31:45 -0500

    user_processing DAG was missing default_args

--- user_processing.py
+++ user_processing.py
@@ -8,8 +8,10 @@ from datetime import datetime
 default_args = {
     'start_date': datetime(2021,1,1)
 }
+
 with DAG('user_processing', 
         schedule_interval='@daily',
+        default_args=default_args,
         catchup=False) as dag:
     # Define tasks/operators
     
