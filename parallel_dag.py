ee256f6ecae5f7d908536f11add1d1974145698d
Geoffrey Clark <g.clark@samsung.com>
2022-03-09 09:17:28 -0500

    Convert task1 & 2 SubDag to TaskGroup

--- parallel_dag.py
+++ parallel_dag.py
@@ -3,6 +3,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.subdag import SubDagOperator
 
 from subdags.subdag_parallel_dag import subdag_parallel_dag
+from airflow.utils.task_group import TaskGroup
 from datetime import datetime
 
 default_args = {
@@ -16,10 +17,16 @@ with DAG(
         catchup=False
         ) as dag:
 
-    processing = SubDagOperator(
-        task_id = 'processing_tasks',
-        subdag = subdag_parallel_dag('parallel_dag', 'processing_tasks', default_args)
-    )
+    with TaskGroup('processing_tasks') as processing_tasks:
+        task_2 = BashOperator(
+            task_id = 'task_2',
+            bash_command = 'sleep 3'
+        )
+
+        task_3 = BashOperator(
+            task_id = 'task_3',
+            bash_command = 'sleep 3'
+        )
 
     task_1 = BashOperator(
         task_id = 'task_1',
@@ -32,6 +39,6 @@ with DAG(
         bash_command = 'sleep 3'
     )
 
-    task_1 >> processing >> task_4
+    task_1 >> processing_tasks >> task_4
 
 
