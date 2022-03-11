60ef725eabe406247c10196a20c791d862135138
Geoffrey Clark <g.clark@samsung.com>
2022-03-11 08:45:52 -0500

    Trigger DAG task runs when dependencies fail

--- trigger_rule.py
+++ trigger_rule.py
@@ -14,13 +14,13 @@ with DAG('trigger_rule',
     
     task_1 = BashOperator(
         task_id = 'task_1',
-        bash_command='exit 0',
+        bash_command='exit 1',
         do_xcom_push = False,
     )
 
     task_2 = BashOperator(
         task_id = 'task_2',
-        bash_command='exit 0',
+        bash_command='exit 1',
         do_xcom_push = False,
     )
 
@@ -28,6 +28,7 @@ with DAG('trigger_rule',
         task_id = 'task_3',
         bash_command='exit 0',
         do_xcom_push = False,
+        trigger_rule='all_failed'
     )
 
     [task_1, task_2] >> task_3
