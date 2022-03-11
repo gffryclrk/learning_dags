85c2dd0f3f5429cd1eb8900b80d251d3922bbc00
Geoffrey Clark <g.clark@samsung.com>
2022-03-11 09:05:25 -0500

    Trigger DAG Task 3 will run when all dependencies complete

--- trigger_rule.py
+++ trigger_rule.py
@@ -20,7 +20,7 @@ with DAG('trigger_rule',
 
     task_2 = BashOperator(
         task_id = 'task_2',
-        bash_command='exit 1',
+        bash_command='exit 0',
         do_xcom_push = False,
     )
 
@@ -28,7 +28,7 @@ with DAG('trigger_rule',
         task_id = 'task_3',
         bash_command='exit 0',
         do_xcom_push = False,
-        trigger_rule='all_failed'
+        trigger_rule='all_done'
     )
 
     [task_1, task_2] >> task_3
